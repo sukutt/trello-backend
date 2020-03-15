45d1ee879a599930f1eae3dc0e11358ea522a071
woongahkim <realsukutt@gmail.com>
2020-03-15 23:06:10 +0900

    Implement OAuth 2.0 Google login

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -89,6 +89,56 @@ exports.signIn = async (ctx) => {
     };
 };
 
+exports.signInWithOAuth = async (ctx) => {
+    const schema = Joi.object({
+        email: Joi.string().email().required(),
+        userId: Joi.string().required()
+    });
+
+    const result = schema.validate(ctx.request.body);
+
+    if(result.error) {
+        ctx.status = 400;
+        return;
+    }
+
+    const { email } = ctx.request.body;
+
+    let account = null;
+    try {
+        account = await Account.findByEmail(email);
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+
+    if (!account) {
+        // 회원가입
+        try {
+            account = await Account.signUp(ctx.request.body);
+        } catch (e) {
+            ctx.throw(500, e);
+        }
+
+        // 계정 마다 폴더 생성
+        const dir = `public/${account.email}/boards`;
+        !fs.existsSync(dir) &&
+        fs.mkdir(dir, { recursive: true }, (err) => { console.log(err); });
+    }
+
+    let token = null;
+    try {
+        token = await account.generateToken();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+
+    ctx.cookies.set('access_token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 7 });
+    ctx.body = { 
+        ...account.profile,
+        email
+    };
+};
+
 exports.exists = async (ctx) => {
     const { key, value } = ctx.params;
     let account = null;
--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -5,6 +5,7 @@ const authCtrl = require('./auth.controller');
 
 auth.post('/signup', authCtrl.signUp);
 auth.post('/signin', authCtrl.signIn);
+auth.post('/signInWithOAuth', authCtrl.signInWithOAuth);
 auth.get('/exists/:key(email|userid)/:value', authCtrl.exists);
 auth.post('/logout', authCtrl.logout);
 auth.get('/check', authCtrl.check);
